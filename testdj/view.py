5e4a0f3e1efc6274ed7357a9dbe81fd13c7098f1
Ihtml <2483690020@qq.com>
2018-11-12 18:33:57 +0800

    first commit

---
+++ testdj/view.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# from django.http import HttpResponse
+# def hello(resquest):
+# 	return HttpResponse('Hello World !')
+# def happy(resquest):
+# 	return HttpResponse('Happy Python !')
+
+from django.shortcuts import render
+ 
+def hello(request):
+    context          = {}
+    context['hello'] = 'Hello World!'
+    # 使用render来替代之前使用的HTTPResponse，使用context做参数
+	# 键值'hello' 对应了模板中的变量"{{hello}}"
+    return render(request, 'hello.html', context)
