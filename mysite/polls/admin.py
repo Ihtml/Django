ffed2c990a73c33e65ea6b227e3a9960ac49ded6
Ihtml <2483690020@qq.com>
2018-11-18 22:30:14 +0800

    custom backend forms

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	# 默认3个关联的选项插槽，切不能移除
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapes']}),
+	]
+	inlines = [ChoiceInline]
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	# 添加‘过滤器’侧边栏，允许以pub_daye字段过滤列表
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
+
+
+
+admin.site.register(Question, QuestionAdmin)
+
