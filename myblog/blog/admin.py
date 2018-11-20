e1b19de3af80068214150646dd1e29d1c216cbf9
Ihtml <2483690020@qq.com>
2018-11-20 20:47:37 +0800

    add admin

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here.zh_Hansfrom .models import Question,
+from .models import Article
+
+admin.site.register(Article)
--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -4,4 +4,6 @@ from django.db import models
 class Article(models.Model):
 	title = models.CharField(max_length=32, default='Title')
 	content = models.TextField(null=True)
-
+	# 定义后台的标题
+	def __str__(self):
+		return self.title
