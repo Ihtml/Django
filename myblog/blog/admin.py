14507fb175b4e65ed13b3c0d6187b36f08549136
Ihtml <2483690020@qq.com>
2018-11-22 23:17:20 +0800

    init bbs project

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.zh_Hansfrom .models import Question,
 from .models import Article
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'content','pub_time')
+    # 添加过滤器，按日期过滤
+    list_filter = ('pub_time',) # 只有一个成员时要加上逗号 ，
+
+admin.site.register(Article, ArticleAdmin)
--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Article(models.Model):
 	title = models.CharField(max_length=32, default='Title')
 	content = models.TextField(null=True)
+	# 创建对象的时候自动获得当前日期
+	pub_time = models.DateTimeField(auto_now=True)
+	# pub_time = models.DateTimeField(null=True)
 	# 定义后台的标题
 	def __str__(self):
 		return self.title
