8eaff860912bf3ae350c7856273f4ad85618a228
Ihtml <2483690020@qq.com>
2018-11-26 01:01:22 +0800

    use object_id in UserMessage model

--- bbs/message/models.py
+++ bbs/message/models.py
@@ -1,11 +1,11 @@
-#
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
 
 
 class UserMessage(models.Model):
-    object_id = models.CharField(max_length=50, default='', primary_key=True, verbose_name=u'主键')
+    object_id = models.CharField(max_length=50,default='', primary_key=True, verbose_name=u"主键")
     name = models.CharField(max_length=20,null=True, blank=True, default="匿名用户", verbose_name=u'用户名')
     email = models.EmailField(verbose_name=u'邮箱')
     address = models.CharField(max_length=100, verbose_name=u'联系地址')
--- bbs/message/views.py
+++ bbs/message/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import UserMessage
 # Create your views here.
+
+
 def getform(request):
+    # queryset类型，支持for循环
+    all_message = UserMessage.objects.all()
+    for message in all_message:
+        print(message)
     return render(request, 'message/message_form.html')
