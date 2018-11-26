8a8b7cc8d322c3917d8f4190c69502552c1a22f1
Ihtml <2483690020@qq.com>
2018-11-26 23:05:14 +0800

    post form data to mysql

--- bbs/message/views.py
+++ bbs/message/views.py
@@ -5,7 +5,21 @@ from .models import UserMessage
 
 def getform(request):
     # queryset类型，支持for循环
-    all_message = UserMessage.objects.all()
-    for message in all_message:
-        print(message)
+    # all_message = UserMessage.objects.filter(name='Foo', address='Beijing')
+    # all_message.delete()
+    # for message in all_message:
+    #     print(message.name)
+    #     message.delete()
+    name = request.POST.get('name', '')
+    message = request.POST.get('message', '')
+    address = request.POST.get('address', '')
+    email = request.POST.get('email', '')
+    user_message = UserMessage()
+    user_message.name = name
+    user_message.message = message
+    user_message.address = address
+    user_message.email = email
+    user_message.object_id = 'helloword2'
+
+    user_message.save()
     return render(request, 'message/message_form.html')
